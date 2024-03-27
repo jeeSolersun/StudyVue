09af4422d56ed2effa9185234f2c6caef88e4b5b
Solersun <solersun@163.com>
2024-03-28 01:50:58 +0800

    hello-world add vuex

--- VueProject/hello-world/src/components/Home.vue
+++ VueProject/hello-world/src/components/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  
+  <div>
+    <h1>我是Home组件</h1>
+    <div>{{this.$store.state.count}}</div>
+    <div>{{this.$store.getters.getPow}}</div>
+    <button @click="addCount">点我使state.count加1</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Home',
+  data() {
+    return {
+      msg: 'hello'
+    }
+  },
+  methods: {
+    addCount() {
+      this.$store.commit('addCount', 1)
+      console.log(this.$store);
+      // console.log(this);
+    }
+  }
 }
 </script>
 
