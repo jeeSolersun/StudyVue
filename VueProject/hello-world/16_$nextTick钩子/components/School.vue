0ab0dd4b34b25fe3b4dde06970ed3e07412d0102
Solersun <solersun@163.com>
2024-04-27 14:25:20 +0800

    16_钩子

---
+++ VueProject/hello-world/16_$nextTick钩子/components/School.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="school">
+    <h1>School组件：演示$nextTick()</h1>
+    <input ref="input1" v-model="msg"/>
+    <button @click="changeMsg">点我changeMsg</button>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'School',
+  components: {
+  },
+  data() {
+    return {
+      msg: 'vue2',
+    }
+  },
+  methods: {
+    changeMsg() {
+      this.msg = 'vue3'
+      // 直接修改value值无效
+      // this.$refs.input1.value = 'go'
+      // 有效
+      this.$nextTick(function () {
+        this.$refs.input1.value = 'go'
+      })
+    }
+  },
+}
+</script>
+
+<style scoped>
+.school {
+  background-color: aqua;
+}
+</style>
--- VueProject/hello-world/src/components/School.vue
+++ VueProject/hello-world/src/components/School.vue
@@ -1,45 +1,24 @@
 <template>
   <div class="school">
-    <h1>School组件</h1>
-    <Student />
-    <h2>来自Student的msg: {{ msg }}</h2>
-    <!-- 取消订阅 -->
-    <button @click="cancelSubscrib">取消订阅</button>
+    <h1>School组件：演示vue-resouce</h1>
+    
   </div>
 </template>
 
 <script>
-// 引入PubSub
-import PubSub from 'pubsub-js'
-import Student from './Student.vue'
+
 
 export default {
   name: 'School',
   components: {
-    Student
   },
   data() {
     return {
-      msg: ''
+      msg: 'vue2',
     }
-  },
-  created() {
-    // 订阅消息
-    this.subToken = PubSub.subscribe('sendMsg', (msg , data) => {
-      console.log('@@msg %s, %s', msg, data);
-      this.msg = data;
-    });
-    // 只订阅一次
-    /* PubSub.subscribeOnce('sendMsg', (msg, data) => {
-      console.log('@@msg %s, %s', msg, data);
-      this.msg = data;
-    }); */
   },
   methods: {
-    cancelSubscrib() {
-      // 取消订阅
-      PubSub.unsubscribe(this.subToken);
-    }
+  
   },
 }
 </script>
