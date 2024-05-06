3608a24f7c4e9db118578322c96a5c0e0d9db9cc
Solersun <solersun@163.com>
2024-05-07 01:05:00 +0800

    32_src_路由的query参数

---
+++ VueProject/hello-world/32_src_路由的query参数/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>我是APP1</h1>
+    <div class="row">
+      <div class="col-sm-2">
+        <nav class="navbar bg-light navbar-light navbar-light">
+          <!-- Links -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <!-- 使用路由 -->
+              <router-link class="nav-link" to="/home" active-class="active">首页</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about" active-class="active">关于</router-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="col-sm-10">
+        <router-view></router-view>
+
+      </div>
+    </div>
+
+    <!-- 垂直导航栏 -->
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msgFromStudent: ''
+    }
+  },
+  components: {
+  },
+  mounted() {
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.active {
+  background-color: aqua !important;
+}
+</style>
---
+++ VueProject/hello-world/32_src_路由的query参数/pages/home/MessageDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- 获取query参数 -->
+    <h2>{{ $route.query.msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageDetail',
+  mounted() {
+    console.log('@@this.$route', this.$route);
+  },
+}
+</script>
+
+<style>
+
+</style>
