244f2aa08c52152ba544ae08fabb3f17ee8081b0
Solersun <solersun@163.com>
2024-05-19 22:50:15 +0800

    41_src_路由独享守卫

---
+++ VueProject/hello-world/41_src_路由独享守卫/pages/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>我是About组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  created() {
+    console.log('@@About组件 created()');
+  },
+  // 路由组件被激活时触发
+  activated() {
+    console.log('@@About组件 activated');
+  },
+  // 路由组件失活时触发
+  deactivated() {
+    console.log('@@About组件 deactivated');
+  }
+}
+</script>
+
+<style>
+
+</style>
