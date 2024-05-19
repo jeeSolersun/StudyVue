bb6e3bf63d39b134d2656b656b18a2e8310d908b
Solersun <solersun@163.com>
2024-05-19 16:53:28 +0800

    36_src_router-link的replace属性

---
+++ VueProject/hello-world/36_src_router-link的replace属性/pages/home/MessageDetail.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- 获取query参数 -->
+    <h1>1、query参数值：</h1>
+    <h2>msg: {{ $route.query.msg }}</h2>
+    <br />
+    <h1>2、params参数：</h1>
+    <h2>id: {{ $route.params.id }}</h2>
+    <h2>title: {{ this.$route.params.title }}</h2>
+    <!-- <h1>user: {{ this.user }}</h1>
+    <button @click="user.name = 'go'">点我change user.name为go</button>
+    <button @click="handleClick">点我change user.name为go</button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageDetail',
+  props: ['lan', 'query', 'params'],
+  data() {
+    return {
+      user: {
+        name: 'codeify'
+      }
+    }
+  },
+  watch: {
+    // user() {
+    //   console.log('@@newVal', newVal);
+    // }
+    user: {
+      deep: true,
+      handler(newVal) {
+        console.log('@@newVal', newVal);
+      }
+    }
+  },
+  mounted() {
+    console.log('@@this.$route', this.$route, this, this.lan);
+  },
+  methods: {
+    handleClick() {
+      this.user.name = 'go'
+    }
+  }
+}
+</script>
+
+<style></style>
