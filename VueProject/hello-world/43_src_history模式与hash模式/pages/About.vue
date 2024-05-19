b1a28af4c429b54ff63b3e9267bda56922919f84
Solersun <solersun@163.com>
2024-05-19 23:11:15 +0800

    43_src_history模式与hash模式

---
+++ VueProject/hello-world/43_src_history模式与hash模式/pages/About.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>我是About组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  created() {
+    console.log('@@About组件 created()');
+  },
+  // 路由组件被激活时触发
+  activated() {
+    console.log('@@About组件 activated');
+  },
+  // 路由组件失活时触发
+  deactivated() {
+    console.log('@@About组件 deactivated');
+  },
+  // 3、组件内的路由守卫
+  // 注意 beforeRouteEnter 是支持给 next 传递回调的唯一守卫。
+  // 对于 beforeRouteUpdate 和 beforeRouteLeave 来说，this 已经可用了，所以不支持传递回调，因为没有必要了。
+  /* beforeRouteEnter(to, from, next) {
+    next(vm => {
+      // 通过 `vm` 访问组件实例
+    })
+  }， */
+  beforeRouteEnter(to, from, next) {
+    // 在渲染该组件的对应路由被 confirm 前调用
+    // 不！能！获取组件实例 `this`
+    // 因为当守卫执行前，组件实例还没被创建
+    console.log('@@beforeRouteEnter About组件');
+    next()
+  },
+  beforeRouteUpdate(to, from, next) {
+    // 在当前路由改变，但是该组件被复用时调用
+    // 举例来说，对于一个带有动态参数的路径 /foo/:id，在 /foo/1 和 /foo/2 之间跳转的时候，
+    // 由于会渲染同样的 Foo 组件，因此组件实例会被复用。而这个钩子就会在这个情况下被调用。
+    // 可以访问组件实例 `this`
+    console.log('@@beforeRouteUpdate About组件');
+    next()
+  },
+  // 这个离开守卫通常用来禁止用户在还未保存修改前突然离开。该导航可以通过 next(false) 来取消。
+  beforeRouteLeave(to, from, next) {
+    // 导航离开该组件的对应路由时调用
+    // 可以访问组件实例 `this`
+    console.log('@@beforeRouteLeave About组件');
+    next()
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VueProject/hello-world/43_src_history模式与hash模式/pages/home/Message.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <h1>/home/message</h1>
+    <ul>
+      <li v-for="item in msgs" :key="item.id">
+        <!-- 传递query参数 -->
+        <!-- <router-link :to="`/home/message/detail?msg=${item.msg}`">{{ item.msg }}</router-link> -->
+        <!-- 传递query参数：对象写法 -->
+        <!-- <router-link :to="{
+          name: 'detail',
+          query: {
+            msg: item.msg
+          }
+        }">{{ item.msg }}</router-link> -->
+        <!-- 传递params参数 -->
+        <!-- <router-link :to="`/home/message/detail/${item.id}/${item.msg}`">{{ item.msg }}</router-link> -->
+        <!-- 传递params参数：对象写法 -->
+        <!-- <router-link replace :to="{
+          // ！！！注意，path与params不能一起，需要使用name或者使用完整的path
+          // path: '/home/message/detail', 
+          name: 'detail',
+          query: {
+            msg: item.msg
+          },
+          params: {
+            id: item.id,
+            title: item.msg,
+            msg22: item.msg
+          },
+        }">{{ item.msg }}</router-link> -->
+        <a href="#" @click="handleClick(item)">{{ item.msg }}</a>
+      </li>
+
+    </ul>
+    <button @click="back">后退</button>
+    <button @click="goForward">前进</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  data() {
+    return {
+      msgs: [
+        { id: 1, msg: 'message001' },
+        { id: 2, msg: 'message002' },
+        { id: 3, msg: 'message003' },
+      ]
+    }
+  },
+  methods: {
+    handleClick(item) {
+      // 编程式导航
+      this.$router.push({
+        name: 'detail',
+        params: {
+          id: item.id,
+          title: item.msg
+        },
+        query: {
+          msg: item.msg
+        }
+      })
+    },
+    goForward() {
+      // 前进，相当于window.history.forward();
+      this.$router.go(1)
+    },
+    back() {
+      // 后退，相当于window.history.back();
+      this.$router.go(-1)
+    }
+  },
+  
+}
+</script>
+
+<style></style>
