8fe9ec79525995d9c21d1044fbb1116e5c58a11f
Solersun <solersun@163.com>
2024-05-19 23:03:34 +0800

    42_src_组件内路由守卫

---
+++ VueProject/hello-world/42_src_组件内路由守卫/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>我是Hello组件</h1>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    id: String
+  },
+  mounted() {
+    // console.log('@@this', this, this.id);
+  },
+}
+</script>
+
+<style>
+
+</style>
