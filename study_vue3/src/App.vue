63a6f5c2ea0abcea7e71f8ee64f479062d6ab29d
Solersun <solersun@163.com>
2024-06-13 00:46:40 +0800

    init study_vue3

--- study_vue3/src/App.vue
+++ study_vue3/src/App.vue
@@ -1,41 +1,47 @@
-<template>
-  <div>{{ name }}--{{ age }}</div>
-  <div>{{info.infoName}}--{{info.msg}}</div>
-  <button @click="getInfo">changInfo</button>
-</template>
-
-<script>
-import { ref } from "vue";
-export default {
-  name: "App",
-  setup() {
-    // 1、基本数据类型
-    let name = ref("vue3");
-    let age = ref(22);
-    // 2、对象类型
-    let info = ref({
-      infoName: 'aa',
-      msg: 'hello vue3-ref'
-    });
-    function getInfo() {
-      // 1、操作普通对象
-      // name = 'vue3 test01';
-      // name.value = 'vue3 test01'
-      // age.value = 23
-      // 2、操作对象
-      info.value.infoName = 'aa test01';
-      info.value.msg = 'hello vue3-ref test01';
-      console.log(name, age);
-    }
-    return {
-      name,
-      age,
-      info,
-      getInfo,
-    };
-  },
-};
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style>
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
